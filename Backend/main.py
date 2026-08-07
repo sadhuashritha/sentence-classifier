2cff7add677843c9a1973d09dbfdbc6cbe38c244
sadhu-ashritha <ashrithasadhu2005@gmail.com>
2026-08-08 01:59:20 +0530

    Enable CORS for deployment

--- Backend/main.py
+++ Backend/main.py
@@ -32,11 +32,10 @@ app.add_middleware(
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 model_path = os.path.join(BASE_DIR, "saved_model", "model.pkl")
 vectorizer_path = os.path.join(BASE_DIR, "saved_model", "vectorizer.pkl")
-
-model = joblib.load(model_path)
-vectorizer = joblib.load(vectorizer_path)
 
 # ==========================================
 # Request Model
